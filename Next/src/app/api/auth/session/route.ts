3b9202a3cc696a5b0814f6a6b0ca60c552826296
jithu5 <abijithr202@gmail.com>
2025-05-07 00:30:16 +0530

    debug session

--- Next/src/app/api/auth/session/route.ts
+++ Next/src/app/api/auth/session/route.ts
@@ -5,6 +5,7 @@ import { authOptions } from '@/lib/auth';
 
 export async function GET() {
     const session = await getServerSession(authOptions);
+    console.log(session)
     if (session?.user.id) {
         return NextResponse.json(session);
     }
